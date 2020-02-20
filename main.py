9fc02e8cc661123ffc44a67f6f9ba607b43d37ee
Vlasov Maksim <maksim20856@gmail.com>
2020-02-20 16:21:23 +0300

    Tetris

---
+++ main.py
@@ -0,0 +1,448 @@
+import pygame
+import random
+import os
+
+pygame.font.init()
+
+# Constants
+
+width = 800
+height = 700
+width_glass = 300
+height_glass = 600
+block_size = 30
+
+top_left_x = (width - width_glass) // 2
+top_left_y = height - height_glass
+
+# Фигуры
+
+S = [['.....',
+      '.....',
+      '..00.',
+      '.00..',
+      '.....'],
+     ['.....',
+      '..0..',
+      '..00.',
+      '...0.',
+      '.....']]
+
+O = [['.....',
+      '.....',
+      '.00..',
+      '.00..',
+      '.....']]
+
+Z = [['.....',
+      '.....',
+      '.00..',
+      '..00.',
+      '.....'],
+     ['.....',
+      '..0..',
+      '.00..',
+      '.0...',
+      '.....']]
+
+I = [['..0..',
+      '..0..',
+      '..0..',
+      '..0..',
+      '.....'],
+     ['.....',
+      '0000.',
+      '.....',
+      '.....',
+      '.....']]
+
+J = [['.....',
+      '.0...',
+      '.000.',
+      '.....',
+      '.....'],
+     ['.....',
+      '..00.',
+      '..0..',
+      '..0..',
+      '.....'],
+     ['.....',
+      '.....',
+      '.000.',
+      '...0.',
+      '.....'],
+     ['.....',
+      '..0..',
+      '..0..',
+      '.00..',
+      '.....']]
+
+L = [['.....',
+      '...0.',
+      '.000.',
+      '.....',
+      '.....'],
+     ['.....',
+      '..0..',
+      '..0..',
+      '..00.',
+      '.....'],
+     ['.....',
+      '.....',
+      '.000.',
+      '.0...',
+      '.....'],
+     ['.....',
+      '.00..',
+      '..0..',
+      '..0..',
+      '.....']]
+
+T = [['.....',
+      '..0..',
+      '.000.',
+      '.....',
+      '.....'],
+     ['.....',
+      '..0..',
+      '..00.',
+      '..0..',
+      '.....'],
+     ['.....',
+      '.....',
+      '.000.',
+      '..0..',
+      '.....'],
+     ['.....',
+      '..0..',
+      '.00..',
+      '..0..',
+      '.....']]
+
+# Список фигур и цветов
+
+shapes = [T, O, I, Z, J, L, S]
+shape_colors = [(0, 255, 0), (255, 0, 0), (0, 255, 255), (255, 255, 0), (255, 165, 0), (0, 0, 255), (128, 0, 128)]
+
+
+# Управление музыкой
+
+def music_play():
+    pygame.mixer.init()
+    pygame.mixer.music.load('music.mp3')
+    pygame.mixer.music.play(loops=-1)
+
+
+def music_stop():
+    pygame.mixer.music.stop()
+
+
+# Прорисовка окна
+
+def draw_window(surface, grid, score=0, last_score=0):
+    surface.fill((42, 100, 120))
+
+    pygame.font.init()
+    font = pygame.font.SysFont('comicsans', 60)
+    label = font.render('Super Tetris', 1, (255, 255, 255))
+
+    surface.blit(label, (top_left_x + width_glass / 2 - (label.get_width() / 2), 30))
+
+    font = pygame.font.SysFont('comicsans', 30)
+    label = font.render('Счет: ' + str(score), 1, (255, 255, 255))
+
+    sx = top_left_x + width_glass + 50
+    sy = top_left_y + height_glass / 2 - 100
+
+    surface.blit(label, (sx + 20, sy + 300))
+
+    label = font.render('Макс. счет: ' + last_score, 1, (255, 255, 255))
+
+    sx = top_left_x - 200
+    sy = top_left_y + 200
+
+    surface.blit(label, (sx + 0, sy + 300))
+
+    for i in range(len(grid)):
+        for j in range(len(grid[i])):
+            pygame.draw.rect(surface, grid[i][j],
+                             (top_left_x + j * block_size, top_left_y + i * block_size, block_size, block_size), 0)
+
+    pygame.draw.rect(surface, (255, 0, 0), (top_left_x, top_left_y, width_glass, height_glass), 5)
+
+    draw_grid(surface, grid)
+
+
+# Загрузка фигур в стакан
+
+class Piece(object):
+    def __init__(self, x, y, shape):
+        self.x = x
+        self.y = y
+        self.shape = shape
+        self.color = shape_colors[random.randint(0, 6)]
+        self.rotation = 0
+
+
+# Создание сетки стакана
+
+def create_grid(locked_pos={}):
+    grid = [[(0, 0, 0) for _ in range(10)] for _ in range(20)]
+
+    for i in range(len(grid)):
+        for j in range(len(grid[i])):
+            if (j, i) in locked_pos:
+                c = locked_pos[(j, i)]
+                grid[i][j] = c
+    return grid
+
+
+# Прорисовка линий сетки
+
+def draw_grid(surface, grid):
+    sx = top_left_x
+    sy = top_left_y
+
+    for i in range(len(grid)):
+        pygame.draw.line(surface, (128, 128, 128), (sx, sy + i * block_size), (sx + width_glass, sy + i * block_size))
+        for j in range(len(grid[i])):
+            pygame.draw.line(surface, (128, 128, 128), (sx + j * block_size, sy),
+                             (sx + j * block_size, sy + height_glass))
+
+
+# Преоброзование фигур
+
+def convert_shape_format(shape):
+    positions = []
+    format = shape.shape[shape.rotation % len(shape.shape)]
+
+    for i, line in enumerate(format):
+        row = list(line)
+        for j, column in enumerate(row):
+            if column == '0':
+                positions.append((shape.x + j, shape.y + i))
+
+    for i, pos in enumerate(positions):
+        positions[i] = (pos[0] - 2, pos[1] - 4)
+
+    return positions
+
+
+# Проверка на правильное движение в пространстве
+
+def valid_movement(shape, grid):
+    accepted_pos = [[(j, i) for j in range(10) if grid[i][j] == (0, 0, 0)] for i in range(20)]
+    accepted_pos = [j for sub in accepted_pos for j in sub]
+
+    formatted = convert_shape_format(shape)
+
+    for pos in formatted:
+        if pos not in accepted_pos:
+            if pos[1] > -1:
+                return False
+    return True
+
+
+# Проверка на проигрыш (не зашла ли фигура за стакан)
+
+def check_lost(positions):
+    for pos in positions:
+        x, y = pos
+        if y < 1:
+            return True
+
+    return False
+
+
+# Выбор рандомной фигуры
+
+def get_shape():
+    return Piece(5, 0, random.choice(shapes))
+
+
+# Создание текста при начале и проигрыше
+
+def draw_text_middle(surface, text, size, color):
+    font = pygame.font.SysFont("comicsans", size, bold=True)
+    label = font.render(text, 1, color)
+
+    surface.blit(label, (
+        top_left_x + width_glass / 2 - (label.get_width() / 2), top_left_y + height_glass / 2 - label.get_height() / 2))
+
+
+# Очищение линий
+
+def clear_lines(grid, locked):
+    inc = 0
+    for i in range(len(grid) - 1, -1, -1):
+        row = grid[i]
+        if (0, 0, 0) not in row:
+            inc += 1
+            ind = i
+            for j in range(len(row)):
+                try:
+                    del locked[(j, i)]
+                except:
+                    continue
+
+    if inc > 0:
+        for key in sorted(list(locked), key=lambda x: x[1])[::-1]:
+            x, y = key
+            if y < ind:
+                newKey = (x, y + inc)
+                locked[newKey] = locked.pop(key)
+
+    return inc
+
+
+# Изображение новой фигуры
+
+def draw_shape_next(shape, surface):
+    font = pygame.font.SysFont('comicsans', 30)
+    label = font.render('Следующая фигура', 2, (255, 255, 255))
+
+    sx = top_left_x + width_glass + 50
+    sy = top_left_y + height_glass / 2 - 100
+    format = shape.shape[shape.rotation % len(shape.shape)]
+
+    for i, line in enumerate(format):
+        row = list(line)
+        for j, column in enumerate(row):
+            if column == '0':
+                pygame.draw.rect(surface, shape.color,
+                                 (sx + j * block_size, sy + i * block_size, block_size, block_size), 0)
+
+    surface.blit(label, (sx - 15, sy - 30))
+
+
+# Обновление счета в файле
+
+def update_score(nscore):
+    score = max_score()
+
+    with open('max_score.txt', 'w') as f:
+        if int(score) > nscore:
+            f.write(str(score))
+        else:
+            f.write(str(nscore))
+
+
+# Чтение максимального счета в файле
+
+def max_score():
+    with open('max_score.txt', 'r') as f:
+        lines = f.readlines()
+        score = lines[0].strip()
+
+    return score
+
+
+# Главный цикл
+
+def main(start):
+    last_score = max_score()
+    locked_positions = {}
+    grid = create_grid(locked_positions)
+
+    change_piece = False
+    run = True
+    current_piece = get_shape()
+    next_piece = get_shape()
+    clock = pygame.time.Clock()
+    f_time = 0
+    fall_speed = 0.27
+    l_time = 0
+    score = 0
+
+    while run:
+        grid = create_grid(locked_positions)
+        f_time += clock.get_rawtime()
+        l_time += clock.get_rawtime()
+        clock.tick()
+
+        if l_time / 1000 > 5:
+            l_time = 0
+            if l_time > 0.12:
+                l_time -= 0.005
+
+        if f_time / 1000 > fall_speed:
+            f_time = 0
+            current_piece.y += 1
+            if not (valid_movement(current_piece, grid)) and current_piece.y > 0:
+                current_piece.y -= 1
+                change_piece = True
+
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                run = False
+                pygame.display.quit()
+
+            if event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_LEFT:
+                    current_piece.x -= 1
+                    if not (valid_movement(current_piece, grid)):
+                        current_piece.x += 1
+                if event.key == pygame.K_RIGHT:
+                    current_piece.x += 1
+                    if not (valid_movement(current_piece, grid)):
+                        current_piece.x -= 1
+                if event.key == pygame.K_DOWN:
+                    current_piece.y += 1
+                    if not (valid_movement(current_piece, grid)):
+                        current_piece.y -= 1
+                if event.key == pygame.K_UP:
+                    current_piece.rotation += 1
+                    if not (valid_movement(current_piece, grid)):
+                        current_piece.rotation -= 1
+
+        shape_pos = convert_shape_format(current_piece)
+
+        for i in range(len(shape_pos)):
+            x, y = shape_pos[i]
+            if y > -1:
+                grid[y][x] = current_piece.color
+
+        if change_piece:
+            for pos in shape_pos:
+                p = (pos[0], pos[1])
+                locked_positions[p] = current_piece.color
+            current_piece = next_piece
+            next_piece = get_shape()
+            change_piece = False
+            score += clear_lines(grid, locked_positions) * 10
+
+        draw_window(start, grid, score, last_score)
+        draw_shape_next(next_piece, start)
+        pygame.display.update()
+
+        if check_lost(locked_positions):
+            start.fill((0, 0, 0))
+            draw_text_middle(start, "Проигрыш", 80, (137, 63, 69))
+            music_stop()
+            pygame.display.update()
+            pygame.time.delay(1500)
+            run = False
+            update_score(score)
+
+
+# Главное меню
+
+def menu(start):
+    run = True
+    while run:
+        start.fill((255, 153, 0))
+        draw_text_middle(start, 'Нажмите на любую клавишу, чтобы начать играть', 40, (255, 255, 255))
+        pygame.display.update()
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                run = False
+            if event.type == pygame.KEYDOWN:
+                music_play()
+                main(start)
+
+    pygame.display.quit()
+
+
+start = pygame.display.set_mode((width, height))
+pygame.display.set_caption('Super Tetris')
+menu(start)
